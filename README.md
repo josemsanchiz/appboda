8065ba514460c06fd1f6ead1b2c2da61ecfc93a1
josemsanchiz <josem.sanchiz@gmail.com>
2015-05-15 20:19:43 +0200

    Primera subida

---
+++ README.md
@@ -0,0 +1,3 @@
+# pruebasBoda
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/FotosController.js
@@ -0,0 +1,24 @@
+/**
+ * FotosController
+ *
+ * @description :: Server-side logic for managing fotos
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	uploadFile: function (req, res) {
+    req.file('fotoboda').upload({
+      adapter: require('skipper-s3'),
+      key: 'AKIAIMZMFAO55BCBXGLQ',
+      secret: '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',
+      bucket: 'subidafotos'
+    }, function (err, filesUploaded) {
+      if (err) return res.negotiate(err);
+      return res.ok({
+        files: filesUploaded,
+        textParams: req.params.all()
+      });
+    });
+  }
+};
+
